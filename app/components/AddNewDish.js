8573ecfe7af7bce09bb689559184a99d0629652c
VITOR BARBOSA <vitorbarbosa19@gmail.com>
2017-07-18 11:56:24 -0300

    Corrections to mobile experience. Added meta tag for viewport

---
+++ app/components/AddNewDish.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default class AddNewDish extends React.Component {
+	render () {
+		return (
+			<div className='add-new-dish-btn'>
+			</div>
+		)
+	}
+}
--- app/components/App.js
+++ app/components/App.js
@@ -2,11 +2,13 @@ import React from 'react'
 //import components
 import Navbar from './Navbar/Navbar'
 import DishesList from './DishesList/DishesList'
+import AddNewDish from './AddNewDish'
 
 const App = () => (
 	<div>
 		<Navbar />
 		<DishesList />
+		<AddNewDish />
 	</div>
 )
 

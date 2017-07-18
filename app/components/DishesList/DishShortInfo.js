7a98835a10a5eec1617d15e071936456dfa11204
VITOR BARBOSA <vitorbarbosa19@gmail.com>
2017-07-18 19:21:58 -0300

    Backend. Integration with Apollo. Included requests to server/database running on GraphCool

--- app/components/DishesList/DishShortInfo.js
+++ app/components/DishesList/DishShortInfo.js
@@ -1,12 +1,41 @@
 import React from 'react'
+import {
+	gql,
+	graphql
+} from 'react-apollo'
 
-export default class DishShortInfo extends React.Component {
-	render () {
-		return (
-			<div className='dish-short-info'>
-				<p className='dish-short-info-name'>Sushi de salmão maçaricado com trufa branca</p>
-				<p className='dish-short-info-restaurant'>Empório Santa Maria</p>
-			</div>
-		)
+const DishShortInfoQuery = gql`
+	query DishShortInfoQuery {
+		allDishes {
+			dish
+			restaurant
+		}
 	}
+`
+
+const DishShortInfoNoData = ({ data: {loading, error, allDishes} }) => {
+	
+	if (loading) {
+		return <p>Loading...</p>
+	}
+
+	if (error) {
+		return <p>{error.message}</p>
+	}
+
+	return (
+		<ul>
+			{allDishes.map( (dish) => 
+				<li className='dish-short-info'>
+					<p className='dish-short-info-name'>{dish.dish}</p>
+					<p className='dish-short-info-restaurant'>{dish.restaurant}</p>
+				</li>
+			)}
+		</ul>
+	)
+
 }
+
+const DishShortInfo = graphql(DishShortInfoQuery)(DishShortInfoNoData)
+
+export default DishShortInfo
--- app/index.js
+++ app/index.js
@@ -1,9 +1,21 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
+import { 
+	ApolloClient, 
+	createNetworkInterface,
+	ApolloProvider } from 'react-apollo'
 import App from './components/App'
 
+const client = new ApolloClient({
+	networkInterface: createNetworkInterface({
+		uri: 'https://api.graph.cool/simple/v1/Dishes'
+	})
+})
+
 ReactDOM.render(
-	<App />,
+	<ApolloProvider client={client}>
+		<App />
+	</ApolloProvider>,
 	document.getElementById('app')
 )
